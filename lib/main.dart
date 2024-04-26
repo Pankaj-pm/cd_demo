7c15986ec1c82c1e08ee5462b3ae74c7e8f3e0e1
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-04-26 09:24:39 +0530

    step

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class _MyAppState extends State<MyApp> {
   //1 = Light
   //2 = dark
   int themeMode = 0;
+  int ci = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -46,23 +47,116 @@ class _MyAppState extends State<MyApp> {
             )
           ],
         ),
-        body: Column(
-          children: [
-            Text("Title", style: Theme.of(context).textTheme.titleLarge),
-            Text("${getThemeMode(themeMode)}"),
-            Icon(
-              (Theme.of(context).brightness == Brightness.dark) ? Icons.dark_mode : Icons.light_mode,
-              size: 50,
+        body: Theme(
+          data: ThemeData(
+            textTheme: TextTheme(
+              bodyMedium: TextStyle(color: Colors.red, fontSize: 50),
+              // displayMedium: TextStyle(color: Colors.red, fontSize: 50),
+              // bodyText1: TextStyle(color: Colors.red,fontSize: 50),
+              // subtitle1: TextStyle(color: Colors.red,fontSize: 50),
+              // titleMedium: TextStyle(color: Colors.red, fontSize: 50),
+              // headline1: TextStyle(color: Colors.red,fontSize: 50),
+              bodyLarge: TextStyle(color: Colors.red, fontSize: 20),
+              bodySmall: TextStyle(color: Colors.red, fontSize: 20),
+              // bodyText2: TextStyle(color: Colors.red,fontSize: 50),
+              // caption: TextStyle(color: Colors.red,fontSize: 50),
+              displayLarge: TextStyle(color: Colors.red, fontSize: 50),
+              displaySmall: TextStyle(color: Colors.red, fontSize: 50),
+              // headline2: TextStyle(color: Colors.red,fontSize: 50),
+              // headline3:TextStyle(color: Colors.red,fontSize: 50),
+              // bodyText1: TextStyle(color: Colors.red),
+              // bodyText2: TextStyle(color: Colors.red),
             ),
-            Text("Hello All"),
-            InkWell(
-              onTap: () {
-
-              },
-              overlayColor: MaterialStatePropertyAll(Colors.yellow),
-              child: Text("Ok"),
-            ),
-          ],
+            // elevatedButtonTheme:
+            //     ElevatedButtonThemeData(style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.yellow))),
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+                backgroundColor: Colors.white, focusColor: Colors.red, shape: CircleBorder()),
+          ),
+          child: Column(
+            children: [
+              Text("Title", style: Theme.of(context).textTheme.titleLarge),
+              Text("${getThemeMode(themeMode)}"),
+              Icon(
+                (Theme.of(context).brightness == Brightness.dark) ? Icons.dark_mode : Icons.light_mode,
+                size: 50,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Stepper(
+                    currentStep: ci,
+                    onStepTapped: (index) {
+                      ci = index;
+                      setState(() {});
+                      print("Index $index");
+                    },
+                    onStepContinue: () {
+                      if (ci < 3) {
+                        ci++;
+                        setState(() {});
+                      }
+                    },
+                    onStepCancel: () {
+                      if (ci > 0) {
+                        ci--;
+                        setState(() {});
+                      }
+                    },
+                    controlsBuilder: (context, details) {
+                      return Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: details.onStepContinue,
+                            child: Text(details.currentStep == 3 ? "Submit" : "Next"),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          if (details.currentStep != 0)
+                            ElevatedButton(
+                              onPressed: details.onStepCancel,
+                              child: Text("Back"),
+                            )
+                        ],
+                      );
+                    },
+                    // stepIconBuilder: (stepIndex, stepState) {
+                    //   print("stepState.index ${stepState.index}");
+                    //   if (ci == 0) {
+                    //     return Icon(Icons.send);
+                    //   }else {
+                    //     return Icon(Icons.settings);
+                    //   }
+                    // },
+                    // type: StepperType.horizontal,
+                    steps: [
+                      Step(
+                        title: Text("Step 1"),
+                        content: TextFormField(),
+                        label: Icon(Icons.add),
+                        state: StepState.error,
+                        isActive: 0 <= ci,
+                      ),
+                      Step(
+                        title: Text("Step 2"),
+                        content: Text("xyz"),
+                        isActive: 1 <= ci,
+                      ),
+                      Step(
+                        title: Text("Step 3"),
+                        content: Text("aaaa"),
+                        isActive: 2 <= ci,
+                      ),
+                      Step(
+                        title: Text("Step 4"),
+                        content: Text("aaaa"),
+                        isActive: 3 <= ci,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
@@ -96,6 +190,11 @@ class _MyAppState extends State<MyApp> {
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
         backgroundColor: Colors.black,
       ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(Colors.red),
+            foregroundColor: MaterialStatePropertyAll(Colors.red)),
+      ),
     );
   }
 
@@ -108,10 +207,7 @@ class _MyAppState extends State<MyApp> {
         bodyLarge: TextStyle(color: Colors.white, fontSize: 50),
       ),
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: Colors.white,
-        focusColor: Colors.red,
-        shape: CircleBorder()
-      ),
+          backgroundColor: Colors.white, focusColor: Colors.red, shape: CircleBorder()),
     );
   }
 }
